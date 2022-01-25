ac63585e6ae80848305fa5b4771b5cd0da1f745e
Redas Kriksciukaitis <redas.kriksciukaitis@oag.com>
2022-01-25 09:25:22 +0200

    Add validator tests

---
+++ ElevatorApp.Tests/Validators/ElevatorFilterParametersValidatorTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using ElevatorApp.ResourceParameters;
+using ElevatorApp.Validators;
+using FluentAssertions;
+using FluentValidation.TestHelper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ElevatorApp.Tests.Validators;
+
+[TestClass]
+public class ElevatorFilterParametersValidatorTests
+{
+    [TestMethod]
+    [DynamicData(nameof(GetTestData), DynamicDataSourceType.Method)]
+    public void ElevatorsFilterParametersValidator_ProvidedFilter_ShouldValidateCorrectly(ElevatorFilterParameters filter, bool expected)
+    {
+        var validator = new ElevatorsFilterParametersValidator();
+
+        var validationResult = validator.TestValidateAsync(filter);
+
+        validationResult.Result.IsValid.Should().Be(expected);
+    }
+    
+    private static IEnumerable<object[]> GetTestData()
+    {
+        yield return new object[] { new ElevatorFilterParameters() {ElevatorId = 999} };
+    }
+}
