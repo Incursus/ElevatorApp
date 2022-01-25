2d2aed724e9450cf161c17efb93cf42b3e7c3a3d
Redas Kriksciukaitis <redas.kriksciukaitis@oag.com>
2022-01-25 13:53:28 +0200

    Add missing files

---
+++ ElevatorApp.Tests/Services/ElevatorEngineTests.cs
@@ -0,0 +1,28 @@
+﻿using ElevatorApp.Interfaces;
+using ElevatorApp.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace ElevatorApp.Tests;
+
+[TestClass]
+public class ElevatorEngineTests
+{
+    private ElevatorEngine elevatorEngine;
+    private Mock<IThread> threadMock;
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        threadMock = new Mock<IThread>();
+        elevatorEngine = new ElevatorEngine(threadMock.Object, new Mock<IElevatorLogger>().Object);
+    }
+    
+    [TestMethod]
+    public async Task Move_WhenCalled_ShouldProperlyDelay()
+    {
+        elevatorEngine.Move(0, 1);
+
+        threadMock.Verify(x => x.Sleep(It.IsAny<int>()), Times.AtLeastOnce);
+    }
+}
